daa865a0e66a02af2dc2f3ea5ac27a83ee37d02e
Arman-a1996 <a.asatryan1996@gmail.com>
2020-02-08 21:30:25 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading:true,
+    }
+  }
+  componentDidMount() {
+    if(this.state.loading) {
+      setTimeout(() => {
+        this.setState({loading:false});
+      },5000 );
+    }
+  }
+  render() {
+    if(this.state.loading) {
+      return <div> ...Loading </div>;
+    }
+    return <div> Hallo </div>;
+  }
+}
+
+
+export default App;
